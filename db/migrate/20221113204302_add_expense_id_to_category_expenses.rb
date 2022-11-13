63a7bbc6d159422760ece1df9e4af8ceb6a67b36
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-13 23:56:30 +0300

    Remigrate table

--- db/migrate/20221113204302_add_expense_id_to_category_expenses.rb
+++ db/migrate/20221113204302_add_expense_id_to_category_expenses.rb
@@ -1,4 +1,6 @@
 class AddExpenseIdToCategoryExpenses < ActiveRecord::Migration[7.0]
   def change
+    add_column :category_expenses, :expense_id, :int
+    add_column :category_expenses, :category_id, :int
   end
 end
--- db/migrate/20221113204641_add_category_id_to_category_expenses.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryIdToCategoryExpenses < ActiveRecord::Migration[7.0]
-  def change
-    add_column :category_expenses, :category_id, :int
-  end
-end
