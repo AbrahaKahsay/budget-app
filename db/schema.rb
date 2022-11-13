7cf3f9ae50b36026ded6522843463ba743d8bba9
AbrahaKahsay <abrahak81@gmail.com>
2022-11-13 23:56:30 +0300

    Remigrate table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_204641) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_204302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_204641) do
   create_table "category_expenses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "expense_id"
     t.integer "category_id"
   end
 
