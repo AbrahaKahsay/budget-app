831559cd89136a3b7c338dca70095c4eb0e74edd
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-14 22:10:50 +0300

    Add an add catagories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,32 @@
 class CategoriesController < ApplicationController
+    before_action :authenticate_user!
+    # load_and_authorize_resource
     def index;
         @categories = Category.all
     end
 
+    # def show
+    #     @category = Category.find(params[:id])
+    #   end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        @category.user_id = current_user.id
+
+        if @category.save
+            redirect_to categories_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    
+    def category_params
+      params.require(:category).permit(:name, :icon)
+    end
   end
--- app/controllers/category_expenses_controller.rb
+++ app/controllers/category_expenses_controller.rb
@@ -1,4 +1,7 @@
 class CategoryExpensesController < ApplicationController
+    before_action :authenticate_user!
+    # load_and_authorize_resource
+
     def index
     end
   end
