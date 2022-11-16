67d2a3dff629ab5d51e36f1bba6d33e8d99d5e50
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-16 15:52:40 +0300

    Modify index html for expenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Category.create(user_id: 1, name: "Foods", icon: "FoodsIcon")
+Expense.create(user_id: 1, name: "carrot", amount: 5)
