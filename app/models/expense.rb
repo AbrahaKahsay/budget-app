c54fc269f8a363bfd1b5d95533e4d0c5bdb82cfa
[AbrahaKahsay] <[abrahak81@gmail.com]>
2022-11-17 17:20:52 +0300

    Add validations to expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,6 @@
 class Expense < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 
   has_many :category_expenses, dependent: :destroy
   has_many :categories, through: :category_expenses
