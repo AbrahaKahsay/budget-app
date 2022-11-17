0ee2eba9eea19b4ce25bc63a21c181701e9bbd11
AbrahaKahsay <abrahak81@gmail.com>
2022-11-17 17:14:33 +0300

    Modify join table

--- db/migrate/20221113200424_create_category_expenses.rb
+++ db/migrate/20221113200424_create_category_expenses.rb
@@ -1,8 +1,8 @@
 class CreateCategoryExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :category_expenses do |t|
-      t.bigint :expense_id
-      t.bigint :category_id
+      t.belongs_to :category, null: false, foreign_key: true
+      t.belongs_to :expense, null: false, foreign_key: true
       
       t.timestamps
     end
