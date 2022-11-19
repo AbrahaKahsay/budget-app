a0b5e70863deee29a435cb6ba721ac45c9f9c563
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-19 16:59:13 +0000

    fix category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,12 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.where(user_id: current_user.id)
+    @total = {}
+    @categories.each do |category|
+      @total[category.id] = Expense.joins(:category_expenses)
+      .where(category_expenses:{category_id: category.id})
+      .where(user_id: current_user.id).sum(:amount)
+    end
   end
 
   def new; end
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,17 @@
 class ExpensesController < ApplicationController
   def index
-    @categories = Category.where(user_id: current_user.id)
+    @category = Category.find_by(id: params[:category_id])
+    @expenses = Expense.joins(:category_expenses).where(category_expenses:{category_id: params[:category_id]}).where(user_id: current_user.id).order(created_at: :desc)
+    @total = Expense.joins(:category_expenses).where(category_expenses:{category_id: params[:category_id]}).where(user_id: current_user.id).sum(:amount)
+
   end
 
+  
   def new
+  end
+
+  private 
+  def expense_params 
+    params.require(:expense).permit(:name, :amount)
   end
 end
