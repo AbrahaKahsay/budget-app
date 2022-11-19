c1c66182e26cc9e2b902308ce2c5dc465b01bde7
AbrahaKahsay <abrahak81@gmail.com>
2022-11-20 00:55:15 +0300

    Fix rubocop issues

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   before(:each) do
     @user = User.create(
-        name: 'Abraha',
-        email: 'abrahak81@gmail.com',
-        password: '123456'
-      )
+      name: 'Abraha',
+      email: 'abrahak81@gmail.com',
+      password: '123456'
+    )
     @category = Category.create(name: 'Category', icon: 'Icon1', user_id: @user.id)
   end
 
