73b34798a6028d2abc774399ceb960cd2e3da191
AbrahaKahsay <abrahak81@gmail.com>
2022-11-19 20:12:50 +0300

    Modify categories controller for new and create methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,5 +9,23 @@ class CategoriesController < ApplicationController
     end
   end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user_id = current_user.id
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
