1bd6e89b19d3cf8c746a48dd08953d780fb3c211
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-19 22:58:39 +0000

    fix on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
     categories_path
   end
 
+  def after_sign_up_path_for(_resource)
+    categories_path
+  end
+
   def after_sign_out_path_for(_resource)
     root_path
   end
