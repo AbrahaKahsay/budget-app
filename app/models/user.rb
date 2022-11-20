83621d34d6f9b2e05cdc9b8615926aae0305c8bd
AbrahaKahsay <abrahak81@gmail.com>
2022-11-20 06:18:55 +0300

    Add validations for email and passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 
   has_many :categories, dependent: :destroy
   has_many :expenses, dependent: :destroy
